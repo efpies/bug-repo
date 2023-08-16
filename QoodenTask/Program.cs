069a9b749fc15dcc6fcf19e0f00cb75c8859225a
s.tolkachev <semyon991214@outlook.com>
2023-08-16 08:47:35 +0300

    Updating program.cs

--- QoodenTask/Program.cs
+++ QoodenTask/Program.cs
@@ -7,99 +7,97 @@ using QoodenTask.Options;
 using QoodenTask.ServiceInterfaces;
 using QoodenTask.Services;
 
-public class Program
-{
-    public static void Main(string[] args)
-    {
-        var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-        builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-            .AddCookie(options =>
-            {
-                options.Events.OnRedirectToLogin = context =>
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+        {
+            options.Events.OnRedirectToLogin = context =>
                 {
                     context.Response.StatusCode = 401;
                     return Task.CompletedTask;
                 };
-                options.Events.OnRedirectToAccessDenied = context =>
+            options.Events.OnRedirectToAccessDenied = context =>
                 {
                     context.Response.StatusCode = 403;
                     return Task.CompletedTask;
                 };
-                options.ExpireTimeSpan = TimeSpan.FromMinutes(20);
-                options.SlidingExpiration = true;
+            options.ExpireTimeSpan = TimeSpan.FromMinutes(20);
+            options.SlidingExpiration = true;
         
-                options.Cookie.SameSite = SameSiteMode.None;
-                options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
-            });
-        builder.Services.AddSingleton<ExchangeData>();
-        builder.Services.AddScoped<IRateService, RateService>();
-        builder.Services.AddScoped<ICurrencyService, CurrencyService>();
-        builder.Services.AddScoped<IUserService, UserService>();
-        builder.Services.AddScoped<IBalanceService, BalanceService>();
-        builder.Services.AddScoped<ITransactionService, TransactionService>();
-        builder.Services.AddScoped<IDepositeService, DepositeService>();
-        builder.Services.AddHostedService<ExchangeRateGenerator>();
-        builder.Services.AddHostedService<MigrationService>();
-        builder.Services.AddDbContextFactory<AppDbContext>(
-            options =>
-                options.UseNpgsql(builder.Configuration["DbConnection"]));
-        builder.Services.AddDbContext<AppDbContext>(
-            options =>
-                options.UseNpgsql(builder.Configuration["DbConnection"]));
-        builder.Services.AddControllers();
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen(opt => {
-            opt.UseOneOfForPolymorphism();
-
-            opt.SelectDiscriminatorNameUsing(_ => "$type");
-            opt.SelectDiscriminatorValueUsing(subType => subType.BaseType!
-                .GetCustomAttributes<JsonDerivedTypeAttribute>()
-                .FirstOrDefault(x => x.DerivedType == subType)?
-                .TypeDiscriminator!.ToString());
-        });
-
-        builder.Services.AddCors(options => options.AddDefaultPolicy(policy =>
-        {
-            policy.WithOrigins("http://localhost:63342")
-                .AllowAnyHeader()
-                .AllowAnyMethod()
-                .AllowCredentials();
-        }));
-        builder.Services.Configure<RateOptions>(builder.Configuration.GetSection("RateOptions"));
-
-        using (var scope = builder.Services.BuildServiceProvider().CreateScope())
-        {
-            var serviceProvider = scope.ServiceProvider;
-            try
-            {
-                var context = serviceProvider.GetRequiredService<AppDbContext>();
-                context.Database.EnsureCreated();
-            }
-            catch(Exception e) { }
+            options.Cookie.SameSite = SameSiteMode.None;
+            options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
         }
+    );
 
-        var app = builder.Build();
+builder.Services.AddSingleton<ExchangeData>();
+builder.Services.AddScoped<ICurrencyService, CurrencyService>();
+builder.Services.AddScoped<IRateService, RateService>();
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IBalanceService, BalanceService>();
+builder.Services.AddScoped<ITransactionService, TransactionService>();
+builder.Services.AddScoped<IDepositeService, DepositeService>();
+builder.Services.AddHostedService<ExchangeRateGenerator>();
+builder.Services.AddHostedService<MigrationService>();
+builder.Services.AddDbContextFactory<AppDbContext>(
+    options =>
+                options.UseNpgsql(builder.Configuration["DbConnection"]));
+builder.Services.AddDbContext<AppDbContext>(
+    options =>
+                options.UseNpgsql(builder.Configuration["DbConnection"]));
+builder.Services.AddControllers();
 
-// Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(opt => {
+    opt.UseOneOfForPolymorphism();
 
-        app.UseHttpsRedirection();
+    opt.SelectDiscriminatorNameUsing(_ => "$type");
+    opt.SelectDiscriminatorValueUsing(subType => subType.BaseType!
+        .GetCustomAttributes<JsonDerivedTypeAttribute>()
+        .FirstOrDefault(x => x.DerivedType == subType)?
+        .TypeDiscriminator!.ToString());
+});
 
-        app.UseCors();
+builder.Services.AddCors(options => options.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins("http://localhost:63342")
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials();
+    }));
+builder.Services.Configure<RateOptions>(builder.Configuration.GetSection("RateOptions"));
 
-        app.UseAuthentication();
-        app.UseAuthorization();
-
-        app.MapControllers();
-
-        app.Run();
+using (var scope = builder.Services.BuildServiceProvider().CreateScope())
+{
+    var serviceProvider = scope.ServiceProvider;
+    try
+    {
+        var context = serviceProvider.GetRequiredService<AppDbContext>();
+        context.Database.EnsureCreated();
+    }
+    catch (Exception e)
+    {
+        // ignored
     }
 }
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
